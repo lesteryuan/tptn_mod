f1f39b6435ab00b58b070bf7b1a82bdad73391a8
lesteryuan <lyuan@yahoo.com>
2022-04-20 10:51:41 -0400

    load tn model

--- tnmod_Mo.R
+++ tnmod_Mo.R
@@ -1,15 +1,11 @@
-## 4.9.2022: R script for Mo TN model
-
+## TN model for Mo
 tnmod <- function(df1, runmod = T) {
 
     print(nrow(df1))
-
-    nperiod <- 6
     df1$yday.q <- df1$month
     df1$yday.q <- factor(df1$yday.q)
     print(table(df1$yday.q))
 
-    ## drop missings
     incvec <- ! is.na(df1$don) & ! is.na(df1$chl) & ! is.na(df1$din) &
         ! is.na(df1$tn) & ! is.na(df1$doc) & ! is.na(df1$vss)
     df1 <- df1[incvec,]
@@ -30,12 +26,18 @@ tnmod <- function(df1, runmod = T) {
     df1$vss <- df1$vss/mn.val["tss"]
 
     ## drop pn measurements near zero or negative
-    pn <- df1$tn - df1$din - df1$don
-    incvec <- pn < 1e-10
+    df1$pn <- df1$tn - df1$din - df1$don
+    incvec <- df1$pn < 1e-10
     print(sum(incvec))
 
     df1 <- df1[!incvec,]
-    print(nrow(df1))
+
+    ## split observations into data in which 10% measurement error
+    ## applies and data in which 10 ug/L error applies
+    incvec <- df1$pn <= 0.1
+
+    ip1 <- (1:nrow(df1))[!incvec]
+    ip2 <- (1:nrow(df1))[incvec]
 
     modstan <- '
         data {
@@ -44,9 +46,14 @@ tnmod <- function(df1, runmod = T) {
             int lakenum[n];
             int nseas;
             int seasnum[n];
-            vector[n] tn;                // particulate N
-            vector[n] chl;               // Chl
-            vector[n] tss;               // VSS
+
+            vector[n] chl;
+            vector[n] tss;
+            int ns[2];
+            int ip1[ns[1]];
+            int ip2[ns[2]];
+            vector[ns[1]] tn1;            // TN obs for different measurement
+            vector[ns[2]] tn2;            // error models
         }
         parameters {
             real k[3];
@@ -55,7 +62,8 @@ tnmod <- function(df1, runmod = T) {
             vector[nlake] etad1;
             vector[nseas] etad2;
 
-            real<lower = 0> sigtn;
+            real<lower = 0> sigtn1;
+            real<lower = 0> sigtn2;
 
             real muu;
             real<lower =0> sigu;
@@ -72,21 +80,23 @@ tnmod <- function(df1, runmod = T) {
             vector[n] u;
             vector[n] tss_mn;
 
-            vector[nseas] b;
             vector[nlake] d1;
             vector[nseas] d2;
+            vector[nseas] b;
 
-            b = mub + sigb*etab;
+            b = mub +  sigb*etab;
 
-            d1 = mud[1] + etad1*sigd[1];
-            d2 = mud[2] + etad2*sigd[2];
+            d1 = mud[1] + etad1*sigd[1];   // set up lake or month specific
+            d2 = mud[2] + etad2*sigd[2];   // coefficients but only use some
+                                           // depending on the model variant
 
             u = muu + sigu*etau;
 
             for (i in 1:n) {
                 tss_mn[i] = log_sum_exp(b[seasnum[i]] + k[3]*chl[i], u[i]);
-
-                tn_mn[i] = log_sum_exp(mud[1] + k[1]*chl[i],
+                // model specified here is constant coef for phytoplankton
+                // N and month varying coef for VSSnp
+                tn_mn[i] = log_sum_exp(mud[1]+ k[1]*chl[i],
                         d2[seasnum[i]] + k[2]*u[i]);
             }
         }
@@ -96,36 +106,49 @@ tnmod <- function(df1, runmod = T) {
             etad1 ~ normal(0,1);
             etad2 ~ normal(0,1);
 
-            k[3] ~ normal(0.85,0.01);
-            k[1] ~ normal(1,1);
+            k[3] ~ normal(0.85,0.01); // prior for slope of VSS-Chl relationship
+                                      // from analysis of a much larger set
+                                      // of VSS-Chl data
+            k[1] ~ normal(1,1);       // weak priors centered at 1.
             k[2] ~ normal(1,1);
             muu ~ normal(0,3);
             etau ~ normal(0,1);
             sigu ~ cauchy(0,3);
-            sigtss ~ normal(0.1,0.002);
-
+            sigtss ~ normal(0.1,0.002);  // strong prior for measurement error
+                                         // of tss
             mub ~ normal(0,3);
             etab ~ normal(0,1);
             sigb ~ cauchy(0,3);
 
-            sigtn ~ cauchy(0,3);
+            sigtn1 ~ cauchy(0,3);
+            sigtn2 ~ normal(0.1, 0.01);  // strong prior for measurement error
+                                         // for low values of TN
 
             tss ~ student_t(4,tss_mn, sigtss);
-            tn ~ normal(tn_mn, sigtn);
+            tn1 ~ normal(tn_mn[ip1], sigtn1);
+            tn2 ~ normal(exp(tn_mn[ip2]), sigtn2);
         }
     '
     rmsout <- function(x,y) sqrt(sum((x-y)^2, na.rm = T)/
                                      min(sum(! is.na(x)), sum(!is.na(y))))
-    gettn <- function(df, varout) {
+    gettn <- function(df, varout, withu=T) {
         k <- apply(varout$k,2,mean)
         mud <- apply(varout$mud, 2, mean)
         mub <- mean(varout$mub)
         d1 <- apply(varout$d1, 2, mean)
         d2 <- apply(varout$d2, 2, mean)
 
-        u <- exp(apply(varout$u, 2, mean))
+
+        if (withu) {
+            u <- exp(apply(varout$u, 2, mean))
+        }
+        else {
+            u <- df$vss - exp(mub)*df$chl^k[3]
+            u[u <= 0] <- 0
+        }
+
         tnpred <- exp(mud[1])*df$chl^k[1] +
-            exp(d2[df$lakenum])*u^k[2]
+            exp(d2[df$seasnum])*u^k[2]
         return(tnpred)
     }
     extractvars <- c("mud", "k", "u", "mub", "d1", "d2", "sigd")
@@ -140,16 +163,22 @@ tnmod <- function(df1, runmod = T) {
         datstan <- list(n = nrow(df1),
                         nlake = max(df1$lakenum),lakenum = df1$lakenum,
                         nseas = max(df1$seasnum), seasnum = df1$seasnum,
-                        tn = log(df1$tn-df1$dtn),
+
+                        dtn = df1$dtn,
                         chl = log(df1$chl),
-                        tss = log(df1$vss))
+                        tss = log(df1$vss),
+                        ns = c(length(ip1), length(ip2)),
+                        ip1 = ip1,
+                        ip2 = ip2,
+                        tn1 = log(df1$tn-df1$dtn)[ip1],
+                        tn2 = (df1$tn - df1$dtn)[ip2])
         print(str(datstan))
+
 
         fit <- stan(model_code = modstan,
                     data = datstan, iter = 1800, chains = nchains,
                     warmup = 600, thin= 1,
                     control = list(adapt_delta = 0.98, max_treedepth = 14))
-        save(fit, file = "fitout.rda")
         varout <- extract(fit, pars = extractvars)
 
         tnpred <- gettn(df1, varout)
@@ -161,5 +190,7 @@ tnmod <- function(df1, runmod = T) {
         return(varout)
     }
 
+    return()
+
 }
-varout.test<- tnmod(moi3.all, runmod = T)
+varout <- tnmod(moi3.all, runmod = T)
